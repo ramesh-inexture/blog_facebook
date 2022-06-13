7ca5f9e6ee5bedf198fa22019a5304adafd7c427
Ramesh singh Rajpurohit <ramesh.inexture@gmail.com>
2022-06-13 11:35:11 +0530

    changes on envfile and Setting

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-0035-_+n2g8&k-7z4y2h4b7hnn56b+hptltrxzw+l&ztm-u&f='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
