5acbd3ac27b6e13938bab71bfe5e0ad5ea8e5786
Ramesh singh Rajpurohit <ramesh.inexture@gmail.com>
2022-06-21 18:08:31 +0530

    heroku config changes

--- blog/settings.py
+++ blog/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': 5432
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
